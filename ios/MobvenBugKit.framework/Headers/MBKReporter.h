ae91d09708e2bc979545c26429615fecf13b9321
TolgaMobven <t.bagci@mobven.com>
2018-01-18 10:11:20 +0300

    framework updated

--- ios/MobvenBugKit.framework/Headers/MBKReporter.h
+++ ios/MobvenBugKit.framework/Headers/MBKReporter.h
@@ -22,4 +22,17 @@
                  projectId:(NSString *)projectId
        forInvocationOption:(MBKInvocationOption)invocationOption;
 
+/**
+ @param endpoint The address of the endpoint.
+ @param appSecret N/A at this time. Use "1" for now.
+ @param appId N/A at this time. Use "1" for now.
+ @param projectId The token that identifies the app provided by Mobven Technology.
+ @param invocationOption The event types that invoke the bug reporter UI.
+ */
++ (void)startWithEndpoint:(NSString *)endpoint
+                appSecret:(NSString *)appSecret
+                    appId:(NSString *)appId
+                projectId:(NSString *)projectId
+      forInvocationOption:(MBKInvocationOption)invocationOption;
+
 @end
